8130713e0b1e417f3883045df2895eae6233591e
Dirussy <mixam@bell.net>
2025-03-19 12:37:21 -0400

    seperating some code in a function so connect_selected_notify if more readebable #1

--- src/page_consult_client/mod.rs
+++ src/page_consult_client/mod.rs
@@ -2,14 +2,14 @@ mod imp;
 
 use std::{num::NonZero, thread::sleep};
 
-use adw::{prelude::ActionRowExt, subclass::prelude::ObjectSubclassIsExt};
+use adw::{prelude::{ActionRowExt, BinExt}, subclass::prelude::ObjectSubclassIsExt};
 use glib::{Object, clone};
-use gtk::{gio::prelude::{ListModelExt, ListModelExtManual}, glib::{self, object::{Cast, ObjectExt}, types::StaticType}, prelude::WidgetExt};
+use gtk::{glib::{self, object::Cast, types::StaticType}, prelude::WidgetExt};
 // use adw::prelude::*;
 // use adw::subclass::prelude::*;
 
 use crate::sqlite_functions::{self, Client};
-use rusqlite::{self, Connection, params};
+use rusqlite::{self, Connection};
 
 glib::wrapper! {
     pub struct PageConsultClient(ObjectSubclass<imp::PageConsultClient>)
@@ -25,7 +25,6 @@ impl PageConsultClient {
     pub fn setup_list_client(&self){
         let drop_menu = &*self.imp().drop_down_client;
         let address_row = &*self.imp().address_row;
-        let page = (drop_menu,address_row);
 
         let exp = gtk::PropertyExpression::new(
             gtk::StringObject::static_type(),
@@ -34,43 +33,43 @@ impl PageConsultClient {
         );
         drop_menu.set_expression(Some(exp));
 
-        drop_menu.connect_map(move|drop_down|{
-            println!("Update List Client!!");
-            let my_string_vec:Vec<&str> = vec![];
-            let imgs_strlist = gtk::StringList::new(my_string_vec.as_slice()); 
-            drop_down.set_model(Some(&imgs_strlist));
-            
-            let conn = Connection::open("PelouseData.db").expect("Cannot open database");
+        drop_menu.connect_map(clone!(
+            #[weak(rename_to = window)]
+            self, 
+            move|_|{
+                println!("Update List Client!!");
+                let drop_down = &*window.imp().drop_down_client;
+                let my_string_vec:Vec<&str> = vec![];
+                let imgs_strlist = gtk::StringList::new(my_string_vec.as_slice()); 
+                drop_down.set_model(Some(&imgs_strlist));
+                
+                let conn = Connection::open("PelouseData.db").expect("Cannot open database");
 
-            let mut stmt = conn.prepare("SELECT name_client FROM liste_clients").expect("Error from select");
-            let client_iter = stmt.query_map([], |row| {
-                Ok(ClientIter {
-                    name: row.get(0)?,
-                })
-            }).expect("Erreur loading client");
-            for client in client_iter {
-                imgs_strlist.append(&client.unwrap().name);
+                let mut stmt = conn.prepare("SELECT name_client FROM liste_clients").expect("Error from select");
+                let client_iter = stmt.query_map([], |row| {
+                    Ok(ClientIter {
+                        name: row.get(0)?,
+                    })
+                }).expect("Erreur loading client");
+                for client in client_iter {
+                    imgs_strlist.append(&client.unwrap().name);
+                }
+
             }
-
-        });
+        ));
 
         drop_menu.connect_selected_notify(clone!(
             #[weak(rename_to = window)]
             self, move|_|{
-                let drop_down = &*window.imp().drop_down_client;
+                let nom_client = window.get_selected_client_name();
                 let address_row = &*window.imp().address_row;
-                let selectionner = match drop_down.selected_item()
-                {
-                    Some(obj) => obj.downcast::<gtk::StringObject>().unwrap().string().into(),
-                    None => String::from("None")
-                };
-                println!("Client selectionner {}", &selectionner);
-                if selectionner != "None"
+                println!("Client selectionner {}", &nom_client);
+                if nom_client != "None"
                 {
                     let conn = Connection::open("PelouseData.db").expect("Cannot open database");
                     let address: String = conn.query_row_and_then(
                         "SELECT address FROM liste_clients WHERE name_client=?1",
-                        [selectionner],
+                        [nom_client],
                         |row| row.get(0),
                     ).unwrap();
                     println!("{address}");
@@ -79,6 +78,15 @@ impl PageConsultClient {
 
             }
         ));
+    }
+    pub fn get_selected_client_name(&self) -> String {
+        let drop_down = &*self.imp().drop_down_client;
+        let client_name = match drop_down.selected_item()
+        {
+            Some(obj) => obj.downcast::<gtk::StringObject>().unwrap().string().into(),
+            None => String::from("None")
+        };
+        client_name
     }
 }
 
